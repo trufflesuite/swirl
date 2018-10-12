218cf6128dfa75b37e7b50ed8748d5146282fd72
Nicholas J. Paterno <cashlion@protonmail.com>
2018-10-12 00:04:58 -0400

    Update install.sh to handle install/update
    Moved menu_list to separate file to facilitate updates.
    Minor fix to eth_gasPrice 'category'

--- install.sh
+++ install.sh
@@ -2,4 +2,23 @@
 
 mkdir -p ~/bin
 cp swirl ~/bin && chmod +x ~/bin/swirl
-touch ~/.bash_completion && cat swirl_completion >> ~/.bash_completion && source ~/.bashrc
+menu_list=$(cat menu_list)
+touch ~/.bash_completion
+if grep -q "'$menu_list'" ~/.bash_completion
+then # If found
+  echo "Up To Date"
+else
+  old_list="compgen -W '[_ a-zA-Z3]*' -- \$swirl_arg"
+  if grep -q "$old_list" ~/.bash_completion
+  then # If found
+    update="compgen -W '$menu_list' -- \$swirl_arg"
+    sed -i -e "s/$old_list/$update/g" ~/.bash_completion 
+    echo "Updated"
+  else
+    cat swirl_completion >> ~/.bash_completion && sed -i -e "s/__SWIRL_MENU_LIST__/$menu_list/g" ~/.bash_completion
+    echo "Installed"
+  fi
+  source ~/.bashrc
+fi
+
+exit 0
