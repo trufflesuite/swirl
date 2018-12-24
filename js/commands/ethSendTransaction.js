ed2f3a6786b02f06449bb22ec8f2afa1d9115382
Amal Sudama <cds.sudama@gmail.com>
2018-12-23 20:58:41 -0500

    wip

--- js/commands/ethSendTransaction.js
+++ js/commands/ethSendTransaction.js
@@ -3,16 +3,16 @@ const swirl = require('../core')
 const command = 'jswirl (ethSendTransaction | eth_sendTransaction)'
 const { rich } = require('../utils/helper')
 
-const HELPTEXT = `
+const docString = `
 usage:
   ${command} [-h | --help] [--version]
   ${command} (TXOBJECT | (--to=TO --from=FROM --value=VALUE) [--gas=GAS --gasPrice=GASPRICE --data=DATA --nonce=NONCE]) [--dry-run]
 `
 module.exports = async argv => {
-  const args = run(HELPTEXT, { argv: argv, smartOptions: true })
+  const args = run(docString, { argv: argv, smartOptions: true })
 
   if (Object.keys(args).length < 2) {
-    console.log(HELPTEXT)
+    console.log(docString)
     return
   }
   let result = rich(args)
