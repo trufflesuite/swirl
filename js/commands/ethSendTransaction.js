ff63b2e57458214ce6e6cad9bdf001e86890dc0e
Amal Sudama <cds.sudama@gmail.com>
2018-12-27 14:33:56 -0500

    wip: example of transforming args to command production

--- js/commands/ethSendTransaction.js
+++ js/commands/ethSendTransaction.js
@@ -12,14 +12,33 @@ usage:
 module.exports = async argv => {
   const args = run(docString, { argv: argv, smartOptions: true })
 
-  if (Object.keys(args).length < 2) {
-    console.log(docString)
+  const dryRun = '--dry-run' in args
+  delete args['--dry-run']
+
+  let result
+
+  // match TXOBJECT production
+  if ('TXOBJECT' in args) {
+    result = rich(args)
+    if (dryRun) {
+      console.log('dry-run')
+      console.log('eth_sendTransaction: ', JSON.stringify(result))
+      return
+    } else {
+      console.log('invoke rpc call...with')
+      console.log('eth_sendTransaction: ', JSON.stringify(result))
+    }
+  } else {
+    // match alternate production
+    delete args['ethSendTransaction']
+    const params = Object.entries(args)
+                      .reduce((acc, [key, value]) => {
+                         acc[`${key.replace(/--?/, '')}`] = value
+                         return acc
+                         }, {})
+
+    const rpcArgs = { command: 'ethSendTransaction', params }
+    console.log('rpcCall: ', JSON.stringify(rpcArgs))
     return
   }
-  let result = rich(args)
-  if ('--dry-run' in args) {
-    console.log(args)
-    return
-  }
-  console.log(await swirl('eth_sendTransaction', result))
 }
