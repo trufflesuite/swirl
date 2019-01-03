f26305b0620acbea864e141d904ccf898027a82b
Amal Sudama <cds.sudama@gmail.com>
2019-01-03 15:24:11 -0500

    remove -p option alias

--- js/commands/ethAccounts.js
+++ js/commands/ethAccounts.js
@@ -1,13 +1,20 @@
 const { run } = require('neodoc')
 const command = 'jswirl (ethAccounts | eth_accounts)'
 const docString = `
+Get a list of addresses owned by client.
+
 usage:
   ${command} [-h | --help]
   ${command} [options]
 
 Options:
-  -p, --port PORT   Change port [default: 8545]
+  --port PORT       Change port [default: 8545]
   --dry-run         Perform dry run
+
+Examples:
+  $ jswirl ethAccounts
+
+Reference: https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_accounts
 `
 module.exports = argv => {
   const args = run(docString, { argv: argv, smartOptions: true })
