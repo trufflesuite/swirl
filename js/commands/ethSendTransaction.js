56431e3d69f4fd2f1a1d5183f59e4c3aa9d3e820
Amal Sudama <cds.sudama@gmail.com>
2019-01-03 15:24:31 -0500

    add TXOBJECT example to docString

--- js/commands/ethSendTransaction.js
+++ js/commands/ethSendTransaction.js
@@ -17,7 +17,7 @@ Options:
   --from=FROM             The address the transaction is send from.
   --value=VALUE           Integer of the value sent with this transaction
   --gas=GAS               The gas provided for the transaction execution.
-  --gasPrice=GASPRICE:    The gasPrice used for each paid gas
+  --gasPrice=GASPRICE     The gasPrice used for each paid gas
   --data=DATA             The compiled code of a contract OR the hash of the
                           invoked method signature and encoded parameters. For
                           details see Ethereum
@@ -26,9 +26,18 @@ Options:
                           pending transactions that use the same nonce.
 
 Examples:
-  jswirl ethSendTransaction --to 0xd46e8dd67c5d32be8058bb8eb970870f07244567   \\
-                            --from 0xb60e8dd61c5d32be8058bb8eb970870f07233155 \\
-                            --value 0x9184e72a
+  $ jswirl ethSendTransaction --to 0xd46e8dd67c5d32be8058bb8eb970870f07244567   \\
+                              --from 0xb60e8dd61c5d32be8058bb8eb970870f07233155 \\
+                              --value 0x9184e72a
+
+  $ jswirl ethSendTransaction  \\
+           '{ "from": "0xb60e8dd61c5d32be8058bb8eb970870f07233155",
+              "to": "0xd46e8dd67c5d32be8058bb8eb970870f07244567",
+              "gas": "0x76c0",
+              "gasPrice": "0x9184e72a000",
+              "value": "0x9184e72a",
+              "data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
+            }'
 
 Reference: https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_sendtransaction
 
