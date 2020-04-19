42375b5e73d571fcd1d89f9518c0fc75f59c610c
Nicholas J. Paterno <cashlion@protonmail.com>
2020-04-19 09:53:28 -0400

    Add Config class, stable

--- packages/cli/src/utils/config.ts
+++ packages/cli/src/utils/config.ts
@@ -1,28 +1,76 @@
-/* eslint-disable no-console */
-import {config as getConfig} from 'dotenv';
-import envPaths from 'env-paths';
-import mkdirp from 'mkdirp';
-const PATHS = envPaths('swirl');
+import Conf, {Options} from 'conf';
 
-import {existsSync, writeFileSync} from 'fs';
-import {join} from 'path';
-
-export class Config {
-  static current() {
-    mkdirp.sync(PATHS.config);
-    const location = join(PATHS.config, 'CONFIG.env');
-    if (!existsSync(location)) {
-      writeFileSync(location, '');
-    }
-    const config = getConfig({path: location});
-    console.log(config);
-
-    return config;
-  }
-
-  // constructor() {
-
-  // }
+interface Schema {
+  host: string;
+  port: number;
 }
 
-Config.current();
+export class Config {
+  static schema() {
+    return {
+      host: {
+        type: 'string',
+        format: 'uri',
+        default: 'http://0.0.0.0',
+      },
+      port: {
+        type: 'number',
+        minimum: 1024,
+        maximum: 65535,
+        default: 1337,
+      },
+    };
+  }
+
+  static options() {
+    return {
+      schema: this.schema(),
+    } as Options<Schema>;
+  }
+
+  static log(...args: any[]) {
+    // eslint-disable-next-line no-console
+    console.log(...args);
+  }
+
+  static getConfig() {
+    return new Conf<Schema>(this.options());
+  }
+
+  static current(): Schema {
+    const config = this.getConfig();
+    const host = config.get('host');
+    const port = config.get('port');
+    return {host, port};
+  }
+
+  static set<K extends keyof Schema, T extends Schema[K]>(key: K, value: T) {
+    const config = this.getConfig();
+    config.set(key, value);
+  }
+
+  static get<K extends keyof Schema>(key: K) {
+    const config = this.getConfig();
+    return config.get(key);
+  }
+
+  config: Conf<Schema>;
+
+  constructor() {
+    this.config = Config.getConfig();
+  }
+
+  set<K extends keyof Schema, T extends Schema[K]>(key: K, value: T) {
+    this.config.set(key, value);
+  }
+
+  get<K extends keyof Schema>(key: K) {
+    return this.config.get(key);
+  }
+
+  current(): Schema {
+    const host = this.config.get('host');
+    const port = this.config.get('port');
+    return {host, port};
+  }
+}
